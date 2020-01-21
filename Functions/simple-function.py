7db2a9ec8a446f89ca8d61a432cfc17814d11448
nanih98 <49982093+nanih98@users.noreply.github.com>
2020-01-21 07:43:09 +0100

    Create simple-function.py

---
+++ Functions/simple-function.py
@@ -0,0 +1,8 @@
+def mensaje():
+
+    print("Hello how are you?")
+    print("Hola que tal estáis todos")
+    print("Hola, mi nombre es Daniel")
+
+#Call to the function mensaje, that will print all the 3 print.
+mensaje()  
