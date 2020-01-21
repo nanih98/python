28ddd0113f4853cba060302ce400d2c701d83362
nanih98 <49982093+nanih98@users.noreply.github.com>
2020-01-21 07:54:16 +0100

    Create simple-function2.py

---
+++ Functions/simple-function2.py
@@ -0,0 +1,26 @@
+def suma():
+    num1=5
+    num2=7
+    print(num1+num2)
+
+suma()
+
+
+#Or
+
+def suma2(num1,num2):
+    print(num1+num2)
+
+suma2(4,4)
+
+
+#Function with return
+
+
+def suma3(num1,num2):
+    
+    resultado=num1+num2
+
+    return resultado
+
+print(suma3(6,6))
